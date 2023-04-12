6a6a281e80ef29858cc38dee721b8b7e8734674a
Shheezam <123527509+Shheezam@users.noreply.github.com>
2023-04-12 22:04:26 +0800

    PRACTICE: FragmentExample1 Code

---
+++ FragmentExample1/MainActivity.java
@@ -0,0 +1,14 @@
+package ph.stacktrek.novare.gamenovare.par.fragmentexample1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
